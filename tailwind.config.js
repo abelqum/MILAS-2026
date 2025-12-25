cd0030156549906fef411b06c08de388ca6e842e
Abel Mauricio Quiroz Mora <abelquiroz38@gmail.com>
2025-12-24 18:32:15 -0600

    PRUEBA 1

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // OJO AQUÍ: Debe apuntar a la carpeta src y a cualquier archivo html o js dentro
+  content: ["./src/**/*.{html,js}"], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
