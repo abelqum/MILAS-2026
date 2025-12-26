594cdfa07b0d5b760b8e53783fc7e22fc0db5edf
Abel Mauricio Quiroz Mora <abelquiroz38@gmail.com>
2025-12-25 22:01:03 -0600

    menu mejorado con x y hamb

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-'use strict'
+const hambMenu = document.getElementById('hamb');
+const menuResp = document.getElementById('menu-resp');
+const iconOpen = document.getElementById('iconHamb');
+const iconClose = document.getElementById('iconClose');
 
-const hambMenu= document.getElementById('hamb');
-const menuResp= document.getElementById('menu-resp');
-
-hambMenu.addEventListener('click', function(){
-
- menuResp.classList.toggle('opacity-0');
+hambMenu.addEventListener('click', () => {
+  menuResp.classList.toggle('opacity-0');
   menuResp.classList.toggle('translate-x-full');
 
+  iconOpen.classList.toggle('hidden');
+  iconClose.classList.toggle('hidden');
 });
